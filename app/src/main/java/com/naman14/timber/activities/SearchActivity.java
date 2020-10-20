cc0f12e7005933fff8cb1a13f83d1ba5405d7dc4
FannySarb <50083314+FannySarb@users.noreply.github.com>
2020-10-20 02:00:17 -0500

    busqueda albumes y canciones

--- app/src/main/java/com/naman14/timber/activities/SearchActivity.java
+++ app/src/main/java/com/naman14/timber/activities/SearchActivity.java
@@ -207,11 +207,7 @@ public class SearchActivity extends BaseActivity implements SearchView.OnQueryTe
             if (isCancelled()) {
                 return null;
             }
-            List<Artist> artistList = ArtistLoader.getArtists(SearchActivity.this, params[0], 7);
-            if (!artistList.isEmpty()) {
-                results.add(getString(R.string.artists));
-                results.addAll(artistList);
-            }
+
             if (results.size() == 0) {
                 results.add(getString(R.string.nothing_found));
             }
